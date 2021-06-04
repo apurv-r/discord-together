53ab2a9e6e00205e68ab93b7fc2fcb67e41f0ad8
Apurv Rane <apurv.r11@gmail.com>
2021-06-04 14:16:42 +0400

    Added files

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from discordTogether import DiscordTogether
--- setup.py
+++ setup.py
@@ -1,25 +1,42 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
 
-VERSION = '0.0.1'
-DESCRIPTION = 'Utilize Discord party games with discord.py!'
+readme = ''
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
 
-# Setting up
-setup(
-    name="discord-together",
-    version=VERSION,
-    author="Apurv Rane",
-    author_email="apurv.r11@gmail.com",
-    description=DESCRIPTION,
-    packages=find_packages(),
-    install_requires=['requests'],
-    keywords=['discord python', 'discord.py', 'discord activities', 'party games', 'youtube together', 'discord together', 'discord party games', 'discord poker'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+
+setup(name='discordTogether',
+      packages = ['discordTogether'],
+      author='Apurv R',
+      url='https://github.com/apurv-r/discordTogether',
+      project_urls={
+        "Issue tracker": "https://github.com/apurv-r/discordTogether/issues",
+      },
+      download_url="https://github.com/apurv-r/discordTogether/archive/refs/tags/1.0.1.tar.gz",
+      version='1.0.2',
+      license='MIT',
+      description='Utilize the BETA Discord Party Games feature!',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      include_package_data=True,
+      install_requires=requirements,
+      keywords = ['discordpy','discord.py','discord together','discord party games','party games','discord activities','discord','discord ext','discord modules'],
+      python_requires='>=3.8.0',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ],
 )
