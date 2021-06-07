b40aa7ee2407cf3a1b7fc964609ee64e830e34f4
Apurv Rane <74893069+apurv-r@users.noreply.github.com>
2021-06-07 13:36:09 +0400

    Update discordTogetherMain.py

--- discordTogether/discordTogetherMain.py
+++ discordTogether/discordTogetherMain.py
@@ -51,8 +51,8 @@ class DiscordTogether():
         Returns:
                 invite_link (str): A discord invite link which, upon clicked, starts the custom activity in the VC.
         '''
-        
-        if option and (option.lower().replace(" ","") in defaultApplications.keys()):
+        # Pre Defined Application ID
+        if option and (str(option).lower().replace(" ","") in defaultApplications.keys()):
             async with aiohttp.ClientSession() as session:         # Credits to VineyS for updating code with aiohttp
                 async with session.post(f"https://discord.com/api/v8/channels/{voiceChannelID}/invites",
                                 json={
@@ -73,5 +73,31 @@ class DiscordTogether():
                 raise ConnectionError("An error occured while retrieving data from Discord API.")
             else:
                 return f"https://discord.com/invite/{result['code']}"
+
+        # User Defined Application ID
+        elif option and (str(option).lower().replace(""," ") not in defaultApplications.keys()):
+            async with aiohttp.ClientSession() as session:         # Credits to VineyS for updating code with aiohttp
+                async with session.post(f"https://discord.com/api/v8/channels/{voiceChannelID}/invites",
+                                json={
+                                    'max_age': 86400,
+                                    'max_uses': 0,
+                                    'target_application_id': str(option),
+                                    'target_type': 2,
+                                    'temporary': False,
+                                    'validate': None
+                                }, 
+                                headers = {
+                                    'Authorization': f'Bot {self.client.http.token}',
+                                    'Content-Type': 'application/json'
+                                }
+                            ) as resp:
+                    result = await resp.json()
+            if ("errors" in result.keys()) or ("code" not in result.keys()):
+                if "target_application_id" in result['errors'].keys():
+                    raise InvalidArgument(f"\"{str(option)}\" is an invalid custom application ID.")
+                else:
+                    raise ConnectionError("An error occured while retrieving data from Discord API.")
+            else:
+                return f"https://discord.com/invite/{result['code']}"
         else:
-            raise InvalidArgument("Invalid activity option chosen. You may only choose between (\"youtube\",\"poker\",\"chess\",\"fishing\",\"betrayal\")")
+            raise InvalidArgument("Invalid activity option chosen. You may only choose between (\"youtube\",\"poker\",\"chess\",\"fishing\",\"betrayal\") or input a custom application ID as a string.")
