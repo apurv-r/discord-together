1ec9a647603f1f21a34b98387aa896e9f38d56b7
NotFlameDev <78838310+NotFlameDev@users.noreply.github.com>
2021-06-25 13:31:47 +0700

    Changes in DiscordTogether (mainly python sytax)
    1. Removed redundant parentheses 
    2. Found a bug where you cannot use AutoShardedClient and/or AutoShardedBot to use DiscordTogether, and have been fixed.
    3. Changes unnecessary docstring and usage of / to Union[...].
    4. Added except Exception to bare except following PEP 8: E722 saying "A bare except: clause will catch SystemExit and KeyboardInterrupt exceptions".
    5. Docstring should use triple double-quote.
    6. Added some typehints of parameters and annotation.
    7. Fixed some typos: occured to occurred

--- discordTogether/discordTogetherMain.py
+++ discordTogether/discordTogetherMain.py
@@ -1,23 +1,24 @@
 import aiohttp
-from discord import Client, InvalidArgument
-from discord.ext.commands import Bot
+from discord import Client, AutoShardedClient, InvalidArgument
+from discord.ext.commands import Bot, AutoShardedBot
 from typing import Union
 
-defaultApplications = {                  # Credits to RemyK888
-    'youtube':'755600276941176913', 
-    'poker':'755827207812677713',
-    'betrayal':'773336526917861400',
-    'fishing':'814288819477020702',
-    'chess':'832012586023256104'
+defaultApplications = {  # Credits to RemyK888
+    'youtube': '755600276941176913',
+    'poker': '755827207812677713',
+    'betrayal': '773336526917861400',
+    'fishing': '814288819477020702',
+    'chess': '832012586023256104'
 }
 
-class DiscordTogether():
+
+class DiscordTogether:
     """
     Controls invite generations.
     ...
     Attributes
     ----------
-    client/bot : discord.Client/discord.Bot
+    client/bot : Union[discord.Client, discord.AutoShardedClient, commands.Bot, commands.AutoShardedBot]
         The client/bot variable used for your bot project.
     Methods
     -------
@@ -25,79 +26,80 @@ class DiscordTogether():
         Generates a invite link to a VC with the Discord Party VC Feature.
     """
 
-    def __init__(self, client : Union[Client, Bot]):
+    def __init__(self, client: Union[Client, Bot, AutoShardedClient, AutoShardedBot]):
         """
         Constructs necessary discord.Client/discord.bot attribute.
         Parameters
         ----------
-            client/bot : discord.Client/discord.Bot
+            client/bot : Union[discord.Client, discord.AutoShardedClient, commands.Bot, commands.AutoShardedBot]
                 The client/bot variable used for your bot project.
         """
 
         if client:
             try:
                 self.client = client
-            except:
+            except Exception:
                 raise AttributeError("The client variable inputted has no \"token\" attribute.")
         else:
             raise ValueError("Valid bot token parameter is needed.")
-    
-    async def create_link(self, voiceChannelID, option):
-        '''
+
+    async def create_link(self, voiceChannelID: int, option: str) -> str:
+        """
         Generates a invite link to a VC with the Discord Party VC Feature.
         Parameters:
                 voiceChannelID (int): ID of the voice channel to create the activity for
                 option (str): A option amongst the predefined choices ("youtube","poker","betrayal","fishing","chess")
         Returns:
                 invite_link (str): A discord invite link which, upon clicked, starts the custom activity in the VC.
-        '''
+        """
         # Pre Defined Application ID
-        if option and (str(option).lower().replace(" ","") in defaultApplications.keys()):
-            async with aiohttp.ClientSession() as session:         # Credits to VineyS for updating code with aiohttp
+        if option and (str(option).lower().replace(" ", "") in defaultApplications.keys()):
+            async with aiohttp.ClientSession() as session:  # Credits to VineyS for updating code with aiohttp
                 async with session.post(f"https://discord.com/api/v8/channels/{voiceChannelID}/invites",
-                                json={
-                                    'max_age': 86400,
-                                    'max_uses': 0,
-                                    'target_application_id': defaultApplications[option],
-                                    'target_type': 2,
-                                    'temporary': False,
-                                    'validate': None
-                                }, 
-                                headers = {
-                                    'Authorization': f'Bot {self.client.http.token}',
-                                    'Content-Type': 'application/json'
-                                }
-                            ) as resp:
+                                        json={
+                                            'max_age': 86400,
+                                            'max_uses': 0,
+                                            'target_application_id': defaultApplications[option],
+                                            'target_type': 2,
+                                            'temporary': False,
+                                            'validate': None
+                                        },
+                                        headers={
+                                            'Authorization': f'Bot {self.client.http.token}',
+                                            'Content-Type': 'application/json'
+                                        }
+                                        ) as resp:
                     result = await resp.json()
             if ("errors" in result.keys()) or ("code" not in result.keys()):
-                raise ConnectionError("An error occured while retrieving data from Discord API.")
+                raise ConnectionError("An error occurred while retrieving data from Discord API.")
             else:
                 return f"https://discord.com/invite/{result['code']}"
 
         # User Defined Application ID
-        elif option and (str(option).lower().replace(""," ") not in defaultApplications.keys()):
-            async with aiohttp.ClientSession() as session:         # Credits to VineyS for updating code with aiohttp
+        elif option and (str(option).lower().replace("", " ") not in defaultApplications.keys()):
+            async with aiohttp.ClientSession() as session:  # Credits to VineyS for updating code with aiohttp
                 async with session.post(f"https://discord.com/api/v8/channels/{voiceChannelID}/invites",
-                                json={
-                                    'max_age': 86400,
-                                    'max_uses': 0,
-                                    'target_application_id': str(option),
-                                    'target_type': 2,
-                                    'temporary': False,
-                                    'validate': None
-                                }, 
-                                headers = {
-                                    'Authorization': f'Bot {self.client.http.token}',
-                                    'Content-Type': 'application/json'
-                                }
-                            ) as resp:
+                                        json={
+                                            'max_age': 86400,
+                                            'max_uses': 0,
+                                            'target_application_id': str(option),
+                                            'target_type': 2,
+                                            'temporary': False,
+                                            'validate': None
+                                        },
+                                        headers={
+                                            'Authorization': f'Bot {self.client.http.token}',
+                                            'Content-Type': 'application/json'
+                                        }
+                                        ) as resp:
                     result = await resp.json()
             if ("errors" in result.keys()) or ("code" not in result.keys()):
                 if "target_application_id" in result['errors'].keys():
                     raise InvalidArgument(f"\"{str(option)}\" is an invalid custom application ID.")
                 else:
-                    raise ConnectionError("An error occured while retrieving data from Discord API.")
+                    raise ConnectionError("An error occurred while retrieving data from Discord API.")
             else:
                 return f"https://discord.com/invite/{result['code']}"
         else:
-            raise InvalidArgument("Invalid activity option chosen. You may only choose between (\"youtube\",\"poker\",\"chess\",\"fishing\",\"betrayal\") or input a custom application ID as a string.")
+            raise InvalidArgument(
+                "Invalid activity option chosen. You may only choose between (\"youtube\",\"poker\",\"chess\",\"fishing\",\"betrayal\") or input a custom application ID as a string.")
