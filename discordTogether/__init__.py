f7b08e5ac0999f870c4e5045e79d97160c1b780c
Apurv Rane <74893069+apurv-r@users.noreply.github.com>
2021-07-02 15:19:55 +0400

    Update __init__.py

--- discordTogether/__init__.py
+++ discordTogether/__init__.py
@@ -1,6 +1,14 @@
-from .discordTogetherMain import DiscordTogether
+"""
+discord-together
+~~~~~~~~~~~~~~~~
+
+An unofficial module that allows the use of the Discord's BETA feature, Party Games - like YouTube together or Poker Night. 
+"""
 
 __name__ = "discord-together"
-__version__ = "1.0.7"
 __author__ = "apurv-r"
+__version__ = "1.0.8"
 __license__ = "MIT"
+
+from .discordTogetherMain import DiscordTogether
+from .errors import *
