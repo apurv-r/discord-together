13f4e37589f29bc8e9d9b19d81f6d3d18702e43c
Apurv Rane <74893069+apurv-r@users.noreply.github.com>
2021-07-29 17:28:52 +0400

    v1.1.1 update

--- discordTogether/errors.py
+++ discordTogether/errors.py
@@ -7,3 +7,11 @@ class InvalidChannelID(ClientException):
 class InvalidActivityChoice(ClientException):
     """Raised when an invalid activity choice was entered."""
     pass
+
+class InvalidCustomID(ClientException):
+    """Raised when an invalid custom application ID was entered."""
+    pass
+
+class RangeExceeded(ClientException):
+    """Raised when the allowed ranges for max_age and max_uses parameters were exceeded."""
+    pass
