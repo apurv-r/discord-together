b7548811e0357d6a0f7eb2fc6480b3b1a53607c7
Apurv Rane <74893069+apurv-r@users.noreply.github.com>
2021-10-16 11:49:36 +0400

    v1.2 release

--- discordTogether/errors.py
+++ discordTogether/errors.py
@@ -1,17 +1,15 @@
-from discord import ClientException
-
-class InvalidChannelID(ClientException):
-    """Raised when an invalid channel ID was entered."""
+class InvalidArgument(Exception):
+    """Raised when an invalid argument was entered."""
     pass
 
-class InvalidActivityChoice(ClientException):
-    """Raised when an invalid activity choice was entered."""
-    pass
-
-class InvalidCustomID(ClientException):
-    """Raised when an invalid custom application ID was entered."""
-    pass
-
-class RangeExceeded(ClientException):
+class RangeExceeded(Exception):
     """Raised when the allowed ranges for max_age and max_uses parameters were exceeded."""
+    pass
+
+class BotMissingPerms(Exception):
+    """Raised when your bot does not have CREATE_INSTANT_INVITE permissions."""
+    pass
+
+class ConnectionError():
+    """Raised when a general error occured while communicating with Discord API."""
     pass
