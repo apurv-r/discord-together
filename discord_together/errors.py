d511b8828c040c095395909139ade8f40b601eac
Apurv Rane <apurv.r11@gmail.com>
2021-10-20 12:07:20 +0400

    v1.2.1 update

---
+++ discord_together/errors.py
@@ -0,0 +1,15 @@
+class InvalidArgument(Exception):
+    """Raised when an invalid argument was entered."""
+    pass
+
+class RangeExceeded(Exception):
+    """Raised when the allowed ranges for max_age and max_uses parameters were exceeded."""
+    pass
+
+class BotMissingPerms(Exception):
+    """Raised when your bot does not have CREATE_INSTANT_INVITE permissions."""
+    pass
+
+class ConnectionError(Exception):
+    """Raised when a general error occured while communicating with Discord API."""
+    pass
