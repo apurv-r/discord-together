2c416e94981102b4515159ede1dd8c849559e724
Apurv Rane <74893069+apurv-r@users.noreply.github.com>
2021-11-06 14:56:01 +0400

    [v1.2.2] added two new default activities

--- discord_together/discordTogetherMain.py
+++ discord_together/discordTogetherMain.py
@@ -10,11 +10,15 @@ defaultApplications = {
     'betrayal': '773336526917861400',
     'fishing': '814288819477020702',
     'chess': '832012774040141894',
+    
     # Credits to awesomehet2124
     'letter-tile': '879863686565621790',
     'word-snack': '879863976006127627',
     'doodle-crew': '878067389634314250',
-    'spellcast': '852509694341283871'
+
+    'spellcast': '852509694341283871',
+    'awkword': '879863881349087252',
+    'checkers': '832013003968348200',
 }
 
 
