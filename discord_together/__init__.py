22baee8ae5511657e909d9826e32bd290d8e37f6
Apurv Rane <74893069+apurv-r@users.noreply.github.com>
2022-01-21 15:38:27 +0400

    v1.2.3 bump

--- discord_together/__init__.py
+++ discord_together/__init__.py
@@ -7,7 +7,7 @@ An unofficial module that allows the use of the Discord's BETA feature, Party Ga
 
 __name__ = "discord-together"
 __author__ = "apurv-r"
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 __license__ = "MIT"
 
 from .discordTogetherMain import DiscordTogether
